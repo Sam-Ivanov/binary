f576aa430b5eaf83bb3c774eaf3b0cd07f8c9914
Aleksandr Ivanov <1300087@gmail.com>
2022-05-27 18:57:33 +0300

    +++

--- js/script.js
+++ js/script.js
@@ -6,11 +6,22 @@ link2.addEventListener('click', scrollMain);
 
 
 function scrollMain(e) {
-   e.preventDefault();
-   var container = this.closest('.mainblock');
-   var style = window.getComputedStyle(container);
-   window.scroll({
-       top: style.height.replace(/[^0-9]/g, ''),
-       behavior: 'smooth'
-   });
+    e.preventDefault();
+    var container = this.closest('.mainblock');
+    var style = window.getComputedStyle(container);
+    window.scroll({
+        top: style.height.replace(/[^0-9]/g, ''),
+        behavior: 'smooth'
+    });
 }
+
+$('.filter__item').click(function (event) {
+    var i = $(this).data('filter');
+    if (i == 1) {
+        $('.portfolio__column').show();
+    } else {
+        $('.portfolio__column').hide();
+        $('.portfolio__column.f_' + i).show();
+    }
+    return false;
+});
